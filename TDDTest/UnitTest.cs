a1175136e84a4844ab492883c8d497edacdb6116
Dariusz Wantuch <w59564@student.wsiz.edu.pl>
2022-06-11 16:19:46 +0200

    Adding an additional test for multiplication.

--- TDDTest/UnitTest.cs
+++ TDDTest/UnitTest.cs
@@ -11,6 +11,8 @@ namespace TDDTest
             Dollar dollar = new Dollar(10);
             dollar.Multiplication(5);
             Assert.Equal(50, dollar.amount);
+            dollar.Multiplication(10);
+            Assert.Equal(500, dollar.amount);
         }
     }
 }
