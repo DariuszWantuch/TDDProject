1b9b46cc526bae78781d3febc9a8e9b54bedfeda
Dariusz Wantuch <w59564@student.wsiz.edu.pl>
2022-06-11 16:09:32 +0200

    Dollar test multiplication.

--- TDDTest/UnitTest.cs
+++ TDDTest/UnitTest.cs
@@ -5,9 +5,11 @@ namespace TDDTest
     public class UnitTest
     {
         [Fact]
-        public void Test()
+        public void DolarTestMultiplication()
         {
-
+            Dollar dollar = new Dollar(10);
+            dollar.Multiplication(5);
+            Assert.Equal(50, dollar.ammount);
         }
     }
 }
