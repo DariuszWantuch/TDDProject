92406093453c9fdb94fe062fe9362553a52f20b0
Dariusz Wantuch <w59564@student.wsiz.edu.pl>
2022-06-11 16:36:56 +0200

    Add Franc class.

---
+++ TDDProject/Model/Franc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDProject.Model
+{
+    public class Franc
+    {
+        private int amount;
+
+        public Franc(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public void Multiplication(int multiplier)
+        {
+            amount *= multiplier;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Franc franc = (Franc)obj;
+
+            return amount == franc.amount;
+        }
+
+        public int GetAmount()
+        {
+            return amount;
+        }
+    }
+}
